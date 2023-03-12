d1263e9c4dc3a49cf04d16f43d183b44be406960
IceCang <jason_lzm@163.com>
2023-03-12 19:52:08 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-function g(n,i){return`<span style='color: ${n}; font-family: Consolas;'>${i}</span>`}let h="",uh="",s=!1;function f(){oO(document.getElementById("tit"),50)}function oO(n,i){n.innerHTML=n.innerHTML+g("rebeccapurple","0"),setTimeout(()=>{oi(n,i)},i)}function oi(n,i){n.innerHTML=n.innerHTML+g("white","i"),setTimeout(()=>{om(n,i)},i)}function om(n,i){n.innerHTML=n.innerHTML+g("red","-"),setTimeout(()=>{oS(n,i)},i)}function oS(n,i){n.innerHTML=n.innerHTML+g("orange","S"),setTimeout(()=>{oe(n,i)},i)}function oe(n,i){n.innerHTML=n.innerHTML+g("yellow","e"),setTimeout(()=>{oa(n,i)},i)}function oa(n,i){n.innerHTML=n.innerHTML+g("lime","a"),setTimeout(()=>{or(n,i)},i)}function or(n,i){n.innerHTML=n.innerHTML+g("blue","r"),setTimeout(()=>{oc(n,i)},i)}function oc(n,i){n.innerHTML=n.innerHTML+g("purple","c"),setTimeout(()=>{oh(n,i)},i)}function oh(n,i){n.innerHTML=n.innerHTML+g("saddlebrown","h"),h=n.innerHTML+"<span style='color: white; font-family: Consolas;'>_</span>",uh=n.innerHTML+"<span style='color: white; font-family: Consolas; visibility: hidden;'>_</span>",setInterval(()=>{o_(n)},20*i)}function o_(n){s?n.innerHTML=uh:n.innerHTML=h,s=!s}f();
+function g(c,f) {
+    return `<span style='color: ${c}; font-family: Consolas;'>${f}</span>`;
+}
+
+let h = "", uh = "";
+let s = false;
+
+function f() {
+    oO(document.getElementById('tit'), 50);
+}
+
+function oO(e,t) {
+    e.innerHTML = e.innerHTML + g("rebeccapurple", "0");
+    setTimeout(()=>{oi(e, t)}, t);
+}
+
+function oi(e,t) {
+    e.innerHTML = e.innerHTML + g("white", "i");
+    setTimeout(()=>{om(e, t)}, t);
+}
+
+function om(e,t) {
+    e.innerHTML = e.innerHTML + g("red", "-");
+    setTimeout(()=>{oS(e, t)}, t);
+}
+
+function oS(e,t) {
+    e.innerHTML = e.innerHTML + g("orange", "S");
+    setTimeout(()=>{oe(e, t)}, t);
+}
+
+function oe(e,t) {
+    e.innerHTML = e.innerHTML + g("yellow", "e");
+    setTimeout(()=>{oa(e, t)}, t);
+}
+
+function oa(e,t) {
+    e.innerHTML = e.innerHTML + g("lime", "a");
+    setTimeout(()=>{or(e, t)}, t);
+}
+
+function or(e,t) {
+    e.innerHTML = e.innerHTML + g("blue", "r");
+    setTimeout(()=>{oc(e, t)}, t);
+}
+
+function oc(e,t) {
+    e.innerHTML = e.innerHTML + g("purple", "c");
+    setTimeout(()=>{oh(e, t)}, t);
+}
+
+function oh(e,t) {
+    e.innerHTML = e.innerHTML + g("saddlebrown", "h");
+    h = e.innerHTML + "<span style='color: white; font-family: Consolas;'>_</span>";
+    uh = e.innerHTML + "<span style='color: white; font-family: Consolas; visibility: hidden;'>_</span>";
+    o_(e);
+    setInterval(()=>{o_(e)}, t*20);
+}
+
+function o_(e) {
+    if(s) e.innerHTML = uh;
+    else e.innerHTML = h;
+    s = !s;
+}
+
+f();
